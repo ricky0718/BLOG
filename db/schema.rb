c90c4a86be3c0a6ffe2bb8a9d5128d6ed2d5ead1
ricky0718 <rikiya0781@gmail.com>
2019-12-13 18:05:28 +0900

    miss

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191116103432) do
+ActiveRecord::Schema.define(version: 20191213072802) do
 
   create_table "entries", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
