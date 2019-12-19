5f90dccf2ac3c59a58afa9351aaa6fe88de93805
ricky0718 <rikiya0781@gmail.com>
2019-12-19 16:45:27 +0900

    user

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -4,5 +4,26 @@ class SignupController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+      if @user.save!
+        sign_in User.find(@user.id) unless user_signed_in?
+        redirect_to root_path(@user.id)
+      else
+        render signup_index_path
+      end
+    end
+  
+    private
+  
+    def user_params
+      params.require(:user).permit(
+      :name, 
+      :email, 
+      :password,
+      :password_confirmation,
+      :prefecture_id,
+      :muscle,
+      :muscle_training
+    )
+    end
   end
-end
