91409a5bd9edff7daf1187d8ef3c5c74d654e35a
ricky0718 <rikiya0781@gmail.com>
2020-01-06 10:33:25 +0900

    user＿up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,28 @@
 class UsersController < ApplicationController
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   def show
     @user = User.find(params[:id])
     # @nickname = user.nickname
     @entrys = @user.entrys
-
     # @entrys = Entry.find(params[:id])
     
     # @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email,:prefecture_id,:muscle,:muscle_training,:user_image,:introduce)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
   
   resources :likes, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show]
+  resources :users, only: [:show,:edit, :update]
   resources :signup, only: [:create, :new]
 end
